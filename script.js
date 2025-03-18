9af89f33e775695a829c3ffeb7a444cbbec46248
Yahto Blokken <blokkenyahto7@gmail.com>
2025-03-18 12:49:13 +0100

    Version2.95

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ function ChangePage() {
     let item = input.value.toLowerCase();
 
     if(item == "home"){
-        location.href = "T:/Informatica%206G/website%20steam/Website-Project/home.html"
+        location.href = "home.html"
     } else if(item == "history"){
-        location.href = "T:/Informatica%206G/website%20steam/Website-Project/history.html"
+        location.href = "history.html"
     } else if(item == "controls") {
-        location.href = "T:/Informatica%206G/website%20steam/Website-Project/manualControls.html"
+        location.href = "manualControls.html"
     } else if(item == "camera") {
-        location.href = "T:/Informatica%206G/website%20steam/Website-Project/cameraLive-Feed.html"
+        location.href = "cameraLive-Feed.html"
     } else if(item == "weather") {
-        location.href = "T:/Informatica%206G/website%20steam/Website-Project/weather.html"
+        location.href = "weather.html"
     } else if(item == "video") {
-        location.href = "T:/Informatica%206G/website%20steam/Website-Project/makingOf-Video.html"
+        location.href = "makingOf-Video.html"
     } else if(item == "poster") {
-        location.href = "T:/Informatica%206G/website%20steam/Website-Project/makingOf-Poster.html"
+        location.href = "makingOf-Poster.html"
     } else if(item == "logbook") {
-        location.href = "T:/Informatica%206G/website%20steam/Website-Project/makingOf-Logbook.html"
+        location.href = "makingOf-Logbook.html"
     } else if(item == "faq") {
-        location.href = "T:/Informatica%206G/website%20steam/Website-Project/makingOf-FAQ.html"
+        location.href = "makingOf-FAQ.html"
     }
 }
 
