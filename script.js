7a25e396a3a2be715adfef67c7684518d6c71fb7
Yahto Blokken <blokkenyahto7@gmail.com>
2025-05-12 12:02:18 +0200

    Version2.9991

--- script.js
+++ script.js
@@ -28,3 +28,14 @@ function ChangePageEnter() {
         ChangePage();
     }
 }
+
+/*weather*/
+!function(d,s,id)
+    {var js,fjs=d.getElementsByTagName(s)[0];
+    if(!d.getElementById(id))
+        {js=d.createElement(s);
+            js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';
+            fjs.parentNode.insertBefore(js,fjs);
+        }
+    }
+(document,'script','weatherwidget-io-js');
